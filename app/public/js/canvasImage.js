76b8a24a14380c72a0d8d746badbaa6c7c2a2e11
srt4 <srt4@uw.edu>
2013-03-06 19:51:17 -0800

    Initial commit

---
+++ app/public/js/canvasImage.js
@@ -0,0 +1,49 @@
+/**
+ * Light layer on top of a canvas element to represent an image displayed
+ * within.  Pass in a canvas element and an Image object and you'll see the
+ * image within the canvas element.  Use the provided methods (e.g. blur) to
+ * manipulate it.
+ *
+ * @constructor
+ * @param {HTMLElement} element HTML canvas element.
+ * @param {Image} image Image object.
+ */
+var CanvasImage = function(element, image) {
+    this.image = image;
+    this.element = element;
+    this.element.width = this.image.width;
+    this.element.height = this.image.height;
+    this.context = this.element.getContext("2d");
+    this.context.drawImage(this.image, 0, 0);
+};
+CanvasImage.prototype = {
+    /**
+     * Runs a blur filter over the image.
+     *
+     * @param {int} strength Strength of the blur.
+     */
+    blur: function (strength) {
+        this.context.globalAlpha = 0.5; // Higher alpha made it more smooth
+        // Add blur layers by strength to x and y
+        // 2 made it a bit faster without noticeable quality loss
+        for (var y = -strength; y <= strength; y += 2) {
+            for (var x = -strength; x <= strength; x += 2) {
+                // Apply layers
+                this.context.drawImage(this.element, x, y);
+                // Add an extra layer, prevents it from rendering lines
+                // on top of the images (does makes it slower though)
+                if (x>=0 && y>=0) {
+                    this.context.drawImage(this.element, -(x-1), -(y-1));
+                }
+            }
+        }
+        this.context.globalAlpha = 1.0;
+    }
+};
+
+/**
+ * Initialise an image on the page and blur it.
+ */
+window.onload = function() {
+    $(".blur").each(function(){n=this,e=new Image,e.onload=function(){t=new CanvasImage(n,this),t.blur(4)},e.src=$(this).attr("src")});
+};
---
+++ app/server/modules/post-manager.js
@@ -0,0 +1,26 @@
+var MongoDB 	= require('mongodb').Db;
+var Server 		= require('mongodb').Server;
+var moment 		= require('moment');
+
+var dbPort 		= 27017;
+var dbHost 		= 'localhost';
+var dbName 		= 'node-login';
+
+var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
+db.open(function(e, d){
+    if (e) {
+        console.log(e);
+    }	else{
+        console.log('connected to database :: ' + dbName);
+    }
+});
+var posts = db.collection('posts');
+
+exports.findPosts = function(callback) {
+    posts.find().toArray(callback)
+};
+
+exports.createPost = function(post, callback) {
+    posts.insert(post, {safe:true}, function(error){
+    });
+};
